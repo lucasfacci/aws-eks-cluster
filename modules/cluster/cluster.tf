ff3276e8375d98cacc158245608a76a8978c90f5
lucasfacci <lucasfacci@protonmail.com>
2024-05-05 16:52:57 -0300

    refactor: changing the tags variable and field position

--- modules/cluster/cluster.tf
+++ modules/cluster/cluster.tf
@@ -11,18 +11,18 @@ resource "aws_eks_cluster" "eks_cluster" {
     endpoint_public_access  = true
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
-  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_role_policy_attachment
-  ]
-
   tags = merge(
     var.tags,
     {
       Name = "${var.project_name}-cluster"
     }
   )
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_role_policy_attachment
+  ]
 }
 
 # output "endpoint" {
--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -3,11 +3,6 @@ variable "project_name" {
   description = "Project name that will be used as the resource name prefix."
 }
 
-variable "tags" {
-  type        = map(any)
-  description = "Tags that will be associated with the created resources."
-}
-
 variable "public_subnet_1a" {
   type        = string
   description = "Public subnet AZ 1a to host the EKS cluster."
@@ -16,4 +11,9 @@ variable "public_subnet_1a" {
 variable "public_subnet_1b" {
   type        = string
   description = "Public subnet AZ 1b to host the EKS cluster."
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Tags that will be associated with the created resources."
 }
